33f355c517bca8564504855d48de07fd07c8582d
Rakibul Hussain <rakibulhussain1100@gmail.com>
2026-07-10 17:04:12 +0530

    abcd

--- src/App.jsx
+++ src/App.jsx
@@ -204,6 +204,8 @@ useEffect(() => {
 
   // 2. Main Portfolio Landing View (Fades in perfectly after boot sequence)
   return (
+
+    
     <div className="min-h-screen bg-kali-bg text-slate-400 font-mono text-xs md:text-sm selection:bg-kali-cyan selection:text-kali-bg p-2 sm:p-4 md:p-8 relative animate-fade-in">
 
       {/* Nothing Tech Grid Matrix Background arrays */}
